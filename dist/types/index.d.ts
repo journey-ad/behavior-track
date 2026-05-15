2b98f8ecf16e00220b25bd9404b57a3023ec4e6e
journey-ad <journey-ad@users.noreply.github.com>
2026-05-15 01:49:50 +0000

    deploy: 860ba5d40fa020503dd4efe0fed3b45d75433d2c

--- dist/types/index.d.ts
+++ dist/types/index.d.ts
@@ -1,9 +1,11 @@
+import './polyfill';
+import type { FormDetectorInstance } from './core/sdk';
 import type { SDKConfig, EnvStaticReport, BehaviorStreamReport, FormDetectConfig } from './types';
 declare const BehaviorTrack: {
     init: (config: SDKConfig) => Promise<void>;
     getEnvInfo: () => Promise<EnvStaticReport>;
     onBehaviorReport: (callback: (data: BehaviorStreamReport) => void) => void;
-    detect: (config: FormDetectConfig) => void;
+    createDetector: (config: Omit<FormDetectConfig, "onResult">) => FormDetectorInstance;
     pause: () => void;
     resume: () => void;
     resetSession: () => string;
@@ -14,5 +16,5 @@ declare const BehaviorTrack: {
     };
     destroy: () => void;
 };
-export { BehaviorTrack };
-export type { SDKConfig, EnvStaticReport, BehaviorStreamReport, FormDetectConfig };
+export default BehaviorTrack;
+export type { SDKConfig, EnvStaticReport, BehaviorStreamReport, FormDetectConfig, FormDetectorInstance };
